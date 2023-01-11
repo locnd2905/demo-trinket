64ab053d10e2538eafc545dfb57b81105ecb7120
locnd2905 <locnd@galaxy.com.vn>
2023-01-11 16:41:26 +0700

    change iframe

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ function App() {
   return (
     <div className="App">
       <div>
-      <iframe  title='tinket' src="https://trinket.io/embed/python/37445ab455" width="1200" height="600" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>
-      </div>
+        <iframe src="https://trinket.io/embed/pygame/cc091b76eb" width="1200" height="600" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>      </div>
     </div>
   );
 }
