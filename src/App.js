1957ea3bd268af6577ea2d6d032c5e7be5e57c27
locnd2905 <locnd@galaxy.com.vn>
2023-01-11 16:16:53 +0700

    init cod

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <iframe src="https://trinket.io/embed/python/37445ab455" width="1200" height="600" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>
+      </div>
+    </div>
+  );
+}
+
+export default App;
