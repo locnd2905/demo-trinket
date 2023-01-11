f9b884560bc0f474c726436b7e74ea5f12cd308c
locnd2905 <locnd@galaxy.com.vn>
2023-01-11 16:25:03 +0700

    add tittle iframe

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
   return (
     <div className="App">
       <div>
-        <iframe src="https://trinket.io/embed/python/37445ab455" width="1200" height="600" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>
+      <iframe  title='tinket' src="https://trinket.io/embed/python/37445ab455" width="1200" height="600" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>
       </div>
     </div>
   );
