254dd23c6b306c480e799820373c500cb25793cb
locnd2905 <locnd@galaxy.com.vn>
2023-01-12 17:07:30 +0700

    test post to trinket

--- src/App.js
+++ src/App.js
@@ -1,54 +1,16 @@
 import 'ace-builds/src-noconflict/ace';
 import "ace-builds/src-noconflict/mode-java";
 import "ace-builds/src-noconflict/theme-github";
-import axios from 'axios';
 import "./App.css";
-import { useState, useEffect } from "react";
 
 function App() {
-  const [idFrame, setIdFrame] = useState(null);
-  const postData = async () => {
-    try {
-      var bodyFormData = new FormData();
-      bodyFormData.append('code', `import matplotlib.pyplot as plt
-fig, ax = plt.subplots()
-
-fruits = ['apple', 'blueberry', 'cherry', 'orange']
-counts = [40, 100, 30, 55]
-bar_labels = ['red', 'blue', '_red', 'orange']
-bar_colors = ['tab:red', 'tab:blue', 'tab:red', 'tab:orange']
-
-ax.bar(fruits, counts, label=bar_labels, color=bar_colors)
-
-ax.set_ylabel('fruit supply')
-ax.set_title('Fruit supply by kind and color')
-ax.legend(title='Fruit color')
-
-plt.show()`);
-      bodyFormData.append('name', '');
-      bodyFormData.append('lang', 'pygame');
-      return await axios.post('https://trinket.io/api/trinkets?library=true', bodyFormData,
-        {
-          headers: {
-            'Content-Type': 'multipart/form-data', 'Access-Control-Allow-Origin': '*',
- } }
- )
-   } catch (error) {
-    return null
-   }
-  }
-  useEffect(() => {
-    let { data } = postData()
-    setIdFrame(data?.id)
-  }, []);
-
   return (
     <div className="App">
       <div>
         <iframe
           id='tinket'
           title="tinket"
-          src={`https://trinket.io/embed/pygame/${idFrame}?outputOnly=true&showInstructions=true`}
+          src={`https://trinket.io/embed/pygame/d3497e4811?outputOnly=true&showInstructions=true`}
           width="1200"
           allow='autoplay; encrypted-media'
           height="600"
