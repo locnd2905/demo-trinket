dc586a58b6796813a49eb05a4a3edc2ea1ba0a90
locnd2905 <locnd@galaxy.com.vn>
2023-01-13 11:00:13 +0700

    update UI

--- src/App.js
+++ src/App.js
@@ -6,18 +6,33 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div>
-        <iframe
-          id='tinket'
-          title="tinket"
-          src={`https://trinket.io/embed/pygame/d3497e4811?outputOnly=true&showInstructions=true`}
-          width="1200"
-          allow='autoplay; encrypted-media'
-          height="600"
-          frameBorder="0"
-          marginWidth="0"
-          marginHeight="0"
-          allowFullScreen></iframe>
+      <div className='test'>
+        <div style={{ width: "50%", display: 'flex',flexDirection:'column',justifyContent:'flex-start',textAlign:'left' }}>
+          <div style={{  display: 'flex', flexDirection: 'column' }}>
+            <span style={{ fontWeight: 'bolder' }}>
+              #Project 1
+            </span>
+            <span >
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </span>
+          </div>
+          <div>
+              
+          </div>
+        </div>
+        <div style={{ width: "50%" }}>
+          <iframe
+            id='tinket'
+            title="tinket"
+            src={`https://trinket.io/embed/pygame/d3497e4811?outputOnly=true&showInstructions=true`}
+            allow='autoplay; encrypted-media'
+            frameBorder="0"
+            width='100%'
+            height='100%'
+            marginWidth="0"
+            marginHeight="0"
+            allowFullScreen></iframe>
+        </div>
       </div>
     </div>
 
